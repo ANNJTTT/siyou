d32ea8c35c8853144d3652de3b4a05e9ee5d7b60
ANNJTTT <76141725+ANNJTTT@users.noreply.github.com>
2021-03-14 17:03:16 +0800

    金英杰

---
+++ jinyingjie.js
@@ -0,0 +1,24 @@
+
+if ($request.url.indexOf("/api.jinyingjie.com/live/seriesCourseForJinyingLive") != -1) {
+  re('"is_try_see":"0"@"try_see_time":"\\d+"','"is_try_see":"1"@"try_see_time":"30000"')
+}
+
+
+
+function re() {
+ var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done(body);
+}
+
