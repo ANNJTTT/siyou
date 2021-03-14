a13cd2a87f30f2d7c3e931e123872924a6a76f53
ANNJTTT <76141725+ANNJTTT@users.noreply.github.com>
2021-03-14 17:07:08 +0800

    牙e家vip

---
+++ yaejiavip.js
@@ -0,0 +1,5 @@
+var obj = JSON.parse($response.body);
+obj.listdata[0]["vipexpired"] = "1695052800";
+obj.listdata[0]["vipexpiredcomment"] = "2023-09-19";
+obj.listdata[0]["viplevel"] = "1";
+$done({body: JSON.stringify(obj)});
